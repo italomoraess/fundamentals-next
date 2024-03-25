741cd10e836008f2def1dcea573066f29726cc76
italomoraess <italomoraes.sth@gmail.com>
2024-03-25 16:19:28 -0300

    difference between server components and client components

--- src/app/products/[...data]/page.tsx
+++ src/app/products/[...data]/page.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 interface ProductProps {
   params: {
     data: string[]
@@ -7,11 +9,16 @@ interface ProductProps {
 export default function Product({ params }: ProductProps) {
   const [productId, size, color] = params.data
 
+  function addToCart() {
+    console.log('Produto adicionado ao carrinho')
+  }
   return (
     <div>
       <p>ProductId: {productId}</p>
       <p>Size: {size}</p>
       <p>Color: {color}</p>
+
+      <button onClick={addToCart}>Adicionar ao carrinho</button>
     </div>
   )
 }
